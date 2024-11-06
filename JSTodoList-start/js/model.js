de74dd7ee692c045ddf5b6b285874c24dcda32e1
CarlaTierrablancaR <itzelrojas589@gmail.com>
2024-11-06 15:04:51 -0600

    commit carla T

---
+++ JSTodoList-start/js/model.js
@@ -0,0 +1,72 @@
+export default class Model {
+  constructor() {
+    this.view = null;
+    this.todos = JSON.parse(localStorage.getItem('todos'));
+    if (!this.todos || this.todos.length < 1) {
+      this.todos = [
+        {
+          id: 0,
+          title: 'Learn JS',
+          description: 'Watch JS Tutorials',
+          completed: false,
+          deadline: ''  // Agregamos el campo deadline por defecto
+        }
+      ]
+      this.currentId = 1;
+    } else {
+      this.currentId = this.todos[this.todos.length - 1].id + 1;
+    }
+  }
+
+  setView(view) {
+    this.view = view;
+  }
+
+  save() {
+    localStorage.setItem('todos', JSON.stringify(this.todos));
+  }
+
+  getTodos() {
+    return this.todos.map((todo) => ({ ...todo }));
+  }
+
+  findTodo(id) {
+    return this.todos.findIndex((todo) => todo.id === id);
+  }
+
+  toggleCompleted(id) {
+    const index = this.findTodo(id);
+    const todo = this.todos[index];
+    todo.completed = !todo.completed;
+    this.save();
+  }
+
+  editTodo(id, values) {
+    const index = this.findTodo(id);
+    Object.assign(this.todos[index], values);
+    this.save();
+  }
+
+  // Actualizamos addTodo para que acepte el parámetro deadline
+  addTodo(title, description, deadline) {
+    const todo = {
+      id: this.currentId++,
+      title,
+      description,
+      completed: false,
+      deadline,  // Agregamos la fecha de deadline al nuevo todo
+    }
+
+    this.todos.push(todo);
+    console.log(this.todos);
+    this.save();
+
+    return { ...todo };
+  }
+
+  removeTodo(id) {
+    const index = this.findTodo(id);
+    this.todos.splice(index, 1);
+    this.save();
+  }
+}
